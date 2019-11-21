b126f62c4a984cfc97140e6c4f4ff19e52882f84
tomimarkus991 <tomimarkusalber@gmail.com>
2019-11-21 20:15:35 +0200

    Improved code

--- test/e2e/nightwatch.js
+++ test/e2e/nightwatch.js
@@ -1,12 +1,27 @@
+var conf = require('../../nightwatch.conf.js');
+
 module.exports =
 {
   'nightwatch test': function(browser)
   {
     browser
-      .url('http://neti.ee')
+      .url('https://nightwatchjs.org/')
       .waitForElementVisible('body')
 
-      .pause(5000)
+      .pause(2000)
+      .execute(function()
+      {
+        document.querySelector('a[href=\'/api\']').click();
+      });
+      
+    browser
+      .pause(2000)
+      .execute(function()
+      {
+        document.querySelector('a[href=\'#assert-urlEquals\']').click();
+      });
+    browser
+      .saveScreenshot(conf.imgpath('assertUrl.png'))
+      .pause(1000)
       .end();
-  }
-};
+  }};
--- test/e2e/paevaleht.js
+++ test/e2e/paevaleht.js
@@ -1,3 +1,5 @@
+var conf = require('../../nightwatch.conf.js');
+
 module.exports =
 {
   'päevaleht test': function(browser)
@@ -8,18 +10,23 @@ module.exports =
 
       .assert.title('Eesti Päevaleht')
       .saveScreenshot(conf.imgpath('paevaleht.png'))
-    //   .assert.elementPresent('.row')
-    //   .assert.elementPresent('.col.col-8')
+      .assert.elementPresent('a[href=\'/arvamus\']') // kuna sama hrefiga elemente on veel siis tahaks teha seda kuidagi unikaalseks
+      .assert.elementPresent('.col.col-8')
     //   .useXpath()
     //   .assert.elementPresent('.opinion-web__menu__heading')
     //   .assert.elementPresent('.editors-choice__heading')
     //   .assert.attributeContains('.row', 'href', 'google.com');
-      .useXpath()
-      .click('/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/section/div/div[1]/ul/li[3]/a')
-      
-      .assert.attributeContains('/html/body/div[1]/div/div/div[1]/div/section/div[2]/div[1]/div', 'href', 'faktikontroll')
+      .click('a[href=\'/faktikontroll\']')
 
-      .pause(5000)
+    //test that article list contains word "FAKTIKONTROLL"
+    // .assert.attributeContains('/html/body/div[1]/div/div/div[1]/div/section/div[2]/div[1]/div', 'href', 'faktikontroll')
+
+      .setValue('input[class=\'header-search__input\']', 'kass')
+      .click('button[class=\'header-search__button\']')
+
+      .waitForElementVisible('body')
+      .saveScreenshot(conf.imgpath('kass.png'))
+      .pause(100)
       .end();
   }
 };
