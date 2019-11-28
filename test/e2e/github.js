a7a57d319e540236c69890693f7e090de98a38d3
birgotpt <48119115+birgotpt@users.noreply.github.com>
2019-11-28 15:24:16 +0200

    Kontrolltöö

--- test/e2e/github.js
+++ test/e2e/github.js
@@ -1,12 +1,61 @@
 var conf = require('../../nightwatch.conf.js');
 
+//neti.ee
 module.exports = {
   'Demo test GitHub': function (browser) {
     browser
-      .url('http://www.github.com/dwyl')
-      .waitForElementVisible('body')
-      .assert.containsText('body', 'dwyl.com')
-      .saveScreenshot(conf.imgpath('dwyl.png'))
+      .url('http://neti.ee')
+      .pause(500)
+      .maximizeWindow()
+      .assert.containsText('body', 'Äri ja Reisimine')
+      .pause(500)
+      .waitForElementVisible('a[href="/cgi-bin/teema/ARI/Postiteenused/"]', 1000)
+      .click('a[href="/cgi-bin/teema/ARI/Postiteenused/"]')
+      .pause(500)
+      .assert.containsText('body', 'Kullerteenused')
+      .pause(500)
+      .assert.title('NETI: /ÄRI/Postiteenused')
+      .pause(500)
+      .saveScreenshot(conf.imgpath('pic.png'))
+      .click('a[href="http://www.dhl.ee/"]')
+      .pause(500)
+      .assert.title('Globaalsed logistikateenused – Rahvusvaheline kaubavedu | DHL Home | Eesti')
+      .saveScreenshot(conf.imgpath('pic.png'))
       .end();
     }
   };
+
+//Eesti Päevaleht
+  module.exports = {
+    function (browser) {
+      browser
+        .url('https://epl.delfi.ee')
+        .pause(500)
+        .maximizeWindow()
+        .assert.title('Eesti Päevaleht')
+        .pause(500)
+        .saveScreenshot(conf.imgpath('pic.png'))
+        .assert.elementPresent('a[href="/arvamus"]', 'ArvamusVeeb')
+        .assert.elementPresent('a[href="/arvamus"]', 'Toimetaja valik')
+        .end()
+    }
+  };
+
+  //Nightwatchjs.org
+  module.exports = {
+    function (browser) {
+      browser
+        .url('https://nightwatchjs.org')
+        .pause(500)
+        .maximizeWindow()
+        .pause(500)
+        //.click('a[href="/api"]')
+        .pause(500)
+        .waitForElementVisible('div[class="sample-test"]')
+        //.moveToElement('div[class="sample-test"]',4 , 5)
+        .pause(500)
+        .end();
+        }
+    };
+  
+
